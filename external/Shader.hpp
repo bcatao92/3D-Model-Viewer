85e0eae442120432e7e97741c7081defb10f39fb
César Billalta Yamasato <cesar.yamasato@usp.br>
2024-06-16 19:35:05 -0300

    Fixed Shader.hpp pathing issue

--- external/Shader.hpp
+++ external/Shader.hpp
@@ -1,8 +1,8 @@
 #ifndef SHADER_H
 #define SHADER_H
 
-#include <glad/glad.h>
-#include <glm/glm.hpp>
+#include <glad/include/glad/glad.h>
+#include <glm/glm/glm.hpp>
 #include <string>
 #include <fstream>
 #include <sstream>
