da6f5d800d54958aa3c7684614c3a79e833a7cd0
César Billalta Yamasato <cesar.yamasato@usp.br>
2024-06-19 00:12:46 -0300

    implemented proper model selection on ModelViewer example

--- examples/ModelViewer.cpp
+++ examples/ModelViewer.cpp
@@ -12,11 +12,41 @@
 #include "utils.hpp"
 #include "Scene.hpp"
 
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
+using namespace std;
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);  
+double mouse_sensitivity = 5000.;
 
+bool is_number(const std::string& s)
+{
+    bool foundDot = false;
+    std::string::const_iterator it = s.begin();
+    while (it != s.end() && (std::isdigit(*it) || (*it == '.' && foundDot==false))){ 
+        if(*it == '.') foundDot = true;
+        ++it;
+    }
+    return !s.empty() && it == s.end();
+}
+
+bool processInput(int argc, char **argv){
+    if(argc < 2){
+        cout << "Not enough arguments" << endl;
+    }
+    else if(argc == 2)
+        return true;
+    else if(argc == 3){
+        string mouseSensitivity(argv[2]);
+        if(!is_number(mouseSensitivity)){
+            cout << "Mouse sensitivity must be number" << endl;}
+        else{
+            mouse_sensitivity = atof(argv[2]);
+            return true;
+        }
+    }
+    else
+        cout << "too many arguments" << endl;
+    cout << "USAGE: Sintese <path-to-model> (optional)<mouse-sensitivity>" << endl;
+    return false;
+}
 
 int main(int argc, char **argv)
 {
@@ -25,19 +55,22 @@ int main(int argc, char **argv)
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
 
-    Scene modelViewer("3D model viewer", 800, 600);
+    if(!processInput(argc, argv)) return -1;
 
-    std::string vertexShaderPath = "Shaders/ModelVShader.vs";
-    std::string fragmentShaderPath = "Shaders/ModelFShader.fs";
+    Scene modelViewer("3D model viewer", 800, 600);
+    modelViewer.setSensitivity(mouse_sensitivity);
+
+    string vertexShaderPath = "Shaders/ModelVShader.vs";
+    string fragmentShaderPath = "Shaders/ModelFShader.fs";
     Shader * ModelShader = new Shader(vertexShaderPath.c_str(), fragmentShaderPath.c_str());
 
-    std::cout << "Carregando modelo" << std::endl;
+    cout << "Carregando modelo" << endl;
 
-    std::string path = getPath("model/backpack.obj");
+    string path = getPath(argv[1]);
     Model model(path.c_str(), ModelShader);
     modelViewer.addModel(model);
 
-    std::cout << "Modelo Carregado" << std::endl;
+    cout << "Modelo Carregado" << endl;
 
     double distance = 10.0f;
 
@@ -48,7 +81,7 @@ int main(int argc, char **argv)
     modelViewer.addLight(glm::vec3(0.f,5.f,5.f), glm::vec3(1.f,1.f,1.f));
     modelViewer.setAmbientLight(glm::vec3(1.0f,1.0f,1.0f));
 
-    modelViewer.addBackground("assets/forest-skyboxes/MountainPath");
+    modelViewer.addBackground("forest-skyboxes/MountainPath");
 
     modelViewer.Start();
     return 0;
